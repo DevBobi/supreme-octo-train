fb41c7b8b9ec59986d45049330437d219316d6ae
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-03-25 03:05:01 +0600

    initial commit

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -27,11 +27,10 @@ const Hero = () => {
                         <h1 className="text-3xl lg:text-5xl xl:text-6xl font-normal leading-none ">
                             A strategy, design, and technology partner for forward-thinking companies and societies.
                         </h1>
-                        <p className="text-xl lg:text-3xl mt-6 font-light">
-                            Free landing page template to promote your business startup and generate leads for the
-                            offered services
+                        <p className="text-xl lg:text-3xl mt-20 font-light">
+                            Consultancy and agency services, built on exceptional technological competence.
                         </p>
-                        <p className="mt-8 md:mt-12 border-red py-3">
+                        <p className="mt-5 md:mt-12 border-red">
                             <Button size="lg">Get Started</Button>
                         </p>
                     </div>
